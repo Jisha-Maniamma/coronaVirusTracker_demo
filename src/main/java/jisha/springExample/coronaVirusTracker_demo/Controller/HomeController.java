7f9978bc759ea1cf500ae8b819c9300d67ac0fbd
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-09-11 21:35:20 +0900

    CoronaVirus Tracker

--- src/main/java/jisha/springExample/coronaVirusTracker_demo/Controller/HomeController.java
+++ src/main/java/jisha/springExample/coronaVirusTracker_demo/Controller/HomeController.java
@@ -1,11 +1,16 @@
 package jisha.springExample.coronaVirusTracker_demo.Controller;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@ComponentScan
 @Controller
+@RequestMapping("/home/Covid19/Tracker")
 public class HomeController {
 
-
+@GetMapping("HomePage")
     public String Home(){
         return "Home";
     }
--- src/main/java/jisha/springExample/coronaVirusTracker_demo/Services/CoronaVirusDataService.java
+++ src/main/java/jisha/springExample/coronaVirusTracker_demo/Services/CoronaVirusDataService.java
@@ -27,7 +27,7 @@ public class CoronaVirusDataService {
     @Autowired
     private List<LocationStats> allStats;
     @PostConstruct
-    @Scheduled(cron = "* * * * * *")
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
         List<LocationStats> newStats=new ArrayList<>();
         HttpClient client=HttpClient.newHttpClient();
