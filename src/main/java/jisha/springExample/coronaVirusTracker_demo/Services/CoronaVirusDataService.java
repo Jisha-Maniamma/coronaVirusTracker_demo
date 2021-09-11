0587be15d1acc3e55abf6ead973725b696ce145a
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-09-11 17:46:34 +0900

    Corona Virus Tracker

---
+++ src/main/java/jisha/springExample/coronaVirusTracker_demo/Services/CoronaVirusDataService.java
@@ -0,0 +1,11 @@
+package jisha.springExample.coronaVirusTracker_demo.Services;
+
+
+public class CoronaVirusDataService {
+
+    public static String DATA_SOURCE="https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    public void fetchVirusData(){
+
+    }
+}
