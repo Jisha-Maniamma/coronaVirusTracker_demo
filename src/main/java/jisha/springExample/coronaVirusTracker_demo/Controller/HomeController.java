4415a5d537b5dc9333175fa5b679fe47c049c115
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-09-11 21:22:56 +0900

    CoronaVirus Tracker

---
+++ src/main/java/jisha/springExample/coronaVirusTracker_demo/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package jisha.springExample.coronaVirusTracker_demo.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+
+    public String Home(){
+        return "Home";
+    }
+}
